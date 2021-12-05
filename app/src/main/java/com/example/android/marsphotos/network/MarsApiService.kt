06697d8968f3d465595170df5a2fda2724541c0c
Administrator <godxuche@gmail.com>
2021-12-05 10:09:27 +0100

    created the retrofit object and added the uri for the web service, and the converter factory...

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,4 +1,13 @@
 package com.example.android.marsphotos.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
 // The base url
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
+// Retrofit  builder to create a Retrofit object
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
