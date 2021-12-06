bc47cf26a129d3abfbdf1777ce37125aaf06cbed
Administrator <godxuche@gmail.com>
2021-12-06 05:08:19 +0100

    refactor

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -4,6 +4,8 @@ import com.squareup.moshi.Json
 
 data class MarsPhoto (
     val id: String,
+
+    // use this annotation if you want to use a different variable name
     @Json(name = "img_src")
     val imgSrcUrl: String
         )
